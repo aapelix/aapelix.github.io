9258486d6d3d998d2b9407b6f15fbebebffbfcce
aapelix <115158315+aapelix@users.noreply.github.com>
2023-05-25 15:30:09 +0300

    .

---
+++ scripts/scroll.js
@@ -0,0 +1,13 @@
+var lastScrollTop;
+navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll',function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop > lastScrollTop){
+        navbar.style.top='-80px';
+    }
+    else{
+        navbar.style.top='0';
+    }
+    lastScrollTop = scrollTop;
+});
