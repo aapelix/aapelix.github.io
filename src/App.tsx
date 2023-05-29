e80587a9d1af66d7b56d463e90674df83dbd0780
aapelix <115158315+aapelix@users.noreply.github.com>
2023-05-29 15:19:05 +0300

    Animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import React from "react";
+
 import { motion } from "framer-motion";
+import Typed from "typed.js";
 
 import Navbar from "./components/Navbar";
 import Mobilenavbar from "./components/Mobilenavbar";
@@ -6,15 +9,27 @@ import Mobilenavbar from "./components/Mobilenavbar";
 import logo from "/LogoStroke.svg";
 
 function App() {
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    // @ts-ignore
+    const typed = new Typed(el.current, {
+      strings: ["It's I, Aapelix"],
+      typeSpeed: 50,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
       <Mobilenavbar />
       <div>
         <section className="main">
-          <motion.div animate={{ y: 20 }}>
-            <h1 className="mainani1">It's I, Aapelix</h1>
-          </motion.div>
+          <span ref={el} />
           <h2>Programmer from Finland</h2>
           <motion.div
             whileTap={{ rotate: 180 }}
@@ -24,6 +39,19 @@ function App() {
             <img src={logo} alt="" />
           </motion.div>
         </section>
+        <div className="shape">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
         <section className="who-i-am">
           <motion.div
             initial={{ y: 200 }}
