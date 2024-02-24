bafb8c0f1a56248de7fca2df3dae5376678a5c54
aapelix <115158315+aapelix@users.noreply.github.com>
2024-02-24 17:24:21 +0200

    Added projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import github from "./assets/github-mark-white.svg";
 
 function App() {
   const redirect = () => {
@@ -47,7 +48,7 @@ function App() {
         >
           <a
             className="p-2 hover-underline-animation"
-            href="https://github.com/aapelix/"
+            href="https://x.com/aapelix1/"
           >
             X
           </a>
@@ -73,9 +74,98 @@ function App() {
           </button>
         </motion.button>
 
-        <h1 className="font-extrabold text-4xl mt-96 translate-y-96 pb-20 text-center">
-          You actually thought that I would do something here?
-        </h1>
+        <div className="w-screen flex flex-col items-center">
+          <p className="text-gray-500 mt-44 text-xl">Jump to</p>
+          <div className="flex flex-row flex-wrap justify-center">
+            <a href="#projects">
+              <div className="bg-zinc-900 w-64 text-center py-10 rounded-3xl m-2 mt-5 hover:bg-zinc-800 duration-300">
+                <h1 className="text-3xl">Projects</h1>
+              </div>
+            </a>
+            <a href="#blog">
+              <div className="bg-zinc-900 w-64 text-center py-10 rounded-3xl m-2 mt-5 hover:bg-zinc-700 duration-300">
+                <h1 className="text-3xl">Blog</h1>
+              </div>
+            </a>
+            <a href="">
+              <div className="bg-zinc-900 w-64 text-center py-10 rounded-3xl m-2 mt-5 hover:bg-zinc-600 duration-300">
+                <h1 className="text-3xl">The moon</h1>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div id="projects" className="bg-zinc-900 w-screen mt-24 pb-24">
+          <h1 className="font-extrabold text-5xl mt-36 pb-20 text-center">
+            My projects
+          </h1>
+          <div className="flex w-screen justify-center flex-wrap">
+            {/* PROJECT */}
+
+            <div className="w-96 h-64 rounded-3xl mclc bg-[url('./assets/mclc-2.png')] hover:scale-110 duration-300 m-5 bg-contain">
+              <h1 className="text-3xl text-center m-5 font-bold">MCLC Rust</h1>
+              <div className="bg-zinc-800 h-32 rounded-3xl translate-y-16 duration-300">
+                <div className="m-3">
+                  <p className="m-5 translate-y-2 text-gray-300">
+                    Minecraft Launcher Core Rust is a crate for rust to download
+                    and launch minecraft versions and more!
+                  </p>
+                  <a href="https://github.com/aapelix/mc_launcher_core_rs">
+                    <img
+                      className="w-8 m-5 h-8 -translate-y-2"
+                      src={github}
+                      alt=""
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* PROJECT */}
+
+            <div className="w-96 h-64 rounded-3xl mclc bg-[url('./assets/banner.png')] hover:scale-110 duration-300 m-5 bg-contain">
+              <h1 className="text-3xl text-center m-5 font-bold">
+                Project KTJNKEIO
+              </h1>
+              <div className="bg-zinc-800 rounded-3xl translate-y-16 h-32 duration-300">
+                <div className="m-3">
+                  <p className="m-5 translate-y-2 text-gray-300">
+                    A work in progress wip game made with Rust & Bevy
+                  </p>
+                  <a href="https://github.com/aapelix/project-ktjnkeio">
+                    <img
+                      className="w-8 m-5 h-8 -translate-y-2"
+                      src={github}
+                      alt=""
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* PROJECT */}
+
+            <div className="w-96 h-64 rounded-3xl mclc bg-[url('https://wholesgame.com/wp-content/uploads/Minecraft-wallpaper.jpg')] hover:scale-110 duration-300 m-5">
+              <h1 className="text-3xl text-center m-5 font-bold">Pruschr</h1>
+              <div className="bg-zinc-800 rounded-3xl translate-y-16 h-32 duration-300">
+                <div className="m-3">
+                  <p className="m-5 translate-y-2 text-gray-300">Soon to be</p>
+                  <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
+                    <img
+                      className="w-8 m-5 h-8 -translate-y-2"
+                      src={github}
+                      alt=""
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="blog" className="bg-zinc-800 w-screen mt-0">
+          <h1 className="font-extrabold text-5xl mt-36 pb-20 text-center">
+            Blog soonTM
+          </h1>
+        </div>
       </div>
     </div>
   );
